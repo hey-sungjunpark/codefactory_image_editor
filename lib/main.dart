76c99cc4b1d332cb68b1b8c8e2c8fcf161cad0ba
sungjunpark <spark1498@gmail.com>
2023-09-14 11:22:57 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:codefactory_image_editor/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
+}
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,35 @@
+
+
+import 'package:codefactory_image_editor/component/main_app_bar.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: MainAppBar(
+              onPickImage: onPickImage,
+              onSaveImage: onSaveImage,
+              onDeleteItem: onDeleteItem,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void onPickImage(){}
+
+  void onSaveImage(){}
+
+  void onDeleteItem(){}
+}
